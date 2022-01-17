c04108a1593d8ea6cc8ca7babf9275f6cc2f2528
saqib34566 <57686488+saqib34566@users.noreply.github.com>
2022-01-17 20:38:06 +0000

    Update roomOfBalloons.java

--- roomOfBalloons.java
+++ roomOfBalloons.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner; // Needed to make Scanner available
 
 public class roomOfBalloons
 {
     public static void main(String[] args)
     {
+        //this prints out a warning stating values that should not be entered into the program, which would cause it to not work
         System.out.println("Do not enter negative numbers and entering strings as program will not function " +
                 "\nand enter a decimal value for volume of balloon!");
+        //calling all the input methods for the user to provide room length and balloon volume
         int lengthOfRoom = getLength();
         int widthOfRoom = getWidth();
         int heightOfRoom = getHeight();
@@ -17,40 +19,50 @@ public class roomOfBalloons
         System.out.println("You need " + totalBalloons + " balloons.");
 
     }
-
-    public static int getLength()
-    {
-        int roomLength;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("input room length in cm: ");
-        roomLength = scanner.nextInt();
-
-        return roomLength;
-    }
-
-    public static int getWidth()
-    {
-        int roomWidth;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("input room width in cm: ");
-        roomWidth = scanner.nextInt();
-
-        return roomWidth;
-    }
-
+    
+    //returns the room length given by the user
     public static int getHeight()
     {
-        int roomHeight;
+        String roomHeight;
+        int intRoomHeight;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("input room Height in cm: ");
-        roomHeight = scanner.nextInt();
+        roomHeight = scanner.nextLine();
+        intRoomHeight = Integer.parseInt(roomHeight);
 
-        return roomHeight;
+        return intRoomHeight;
     }
+    
+    //returns the room width given by the user
+    public static int getWidth()
+    {
+        String roomWidth;
+        int intRoomWidth;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("input room width in cm: ");
+        roomWidth = scanner.nextLine();
+        intRoomWidth = Integer.parseInt(roomWidth);
 
+        return intRoomWidth;
+    }
+    
+    //returns the room height given by the user
+    public static int getHeight()
+    {
+        String roomHeight;
+        int intRoomHeight;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("input room Height in cm: ");
+        roomHeight = scanner.nextLine();
+        intRoomHeight = Integer.parseInt(roomHeight);
+
+        return intRoomHeight;
+    }
+    
+    //return the volume of the room in m3 after given 3 differnt values as parameters.
     public static double roomVolume(int length, int width, int height)
     {
         double volume;
@@ -61,7 +73,8 @@ public class roomOfBalloons
 
         return volumeM3;
     }
-
+    
+    //returns the volume of the balloon in m3 given by the user
     public static double getBalloonVolume()
     {
         Scanner scanner = new Scanner(System.in);
@@ -73,6 +86,7 @@ public class roomOfBalloons
         return balloonVolumeM3;
     }
 
+    //returns the amount of balloons needed to to fill up a room for a given volume
     public static int finalRoomCalc(double r_volume, double b_volume)
     {
         double balloonsNeeded = r_volume / b_volume;
@@ -80,3 +94,4 @@ public class roomOfBalloons
         return (int)balloonsNeeded;
     }
 }
+
